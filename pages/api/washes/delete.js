5807bf1794964d57af2912eebe9a7705645586cb
Oscar Figueroa <fisocodes@gmail.com>
2022-06-06 21:01:07 -0700

    Fixes to wash component and deletion for unknown employee.

--- pages/api/washes/delete.js
+++ pages/api/washes/delete.js
@@ -9,26 +9,28 @@ export default async (req, res) => {
         const [statsRows, statsField] = await connection.query('SELECT * FROM employees_stats WHERE id = ?', [wash.employee_id]);
         const stats = statsRows[0];
 
-        switch(wash.service){
-            case 'EXPRESS':
-                const [expressResults, expressFields] = await connection.query('UPDATE employees_stats SET express_units = express_units - 1 WHERE id = ?', [wash.employee_id]);
-                const express_average_time = (stats.express_units * stats.express_average_time - wash.time)/(stats.express_units - 1);
-                const [expressTimeResults, expressTimeFields] = await connection.query('UPDATE employees_stats SET express_average_time = ? WHERE id = ?', [stats.express_units === 1 ? 0 : express_average_time, wash.employee_id]);
-                break;
-            case 'MASTER':
-                const [masterResults, masterFields] = await connection.query('UPDATE employees_stats SET master_units = master_units - 1 WHERE id = ?', [wash.employee_id]);
-                const master_average_time = (stats.master_units * stats.master_average_time - wash.time)/(stats.master_units - 1);
-                const [masterTimeResults, masterTimeFields] = await connection.query('UPDATE employees_stats SET master_average_time = ? WHERE id = ?', [stats.master_units === 1 ? 0 : master_average_time, wash.employee_id]);
-                break;
-            case 'PREMIUM':
-                const [premiumResults, premiumFields] = await connection.query('UPDATE employees_stats SET premium_units = premium_units - 1 WHERE id = ?', [wash.employee_id]);
-                const premium_average_time = (stats.premium_units * stats.premium_average_time - wash.time)/(stats.premium_units - 1);
-                const [premiumTimeResults, premiumTimeFields] = await connection.query('UPDATE employees_stats SET premium_average_time = ? WHERE id = ?', [stats.premium_units === 1 ? 0 : premium_average_time, wash.employee_id]);
-                break;
-            
+        if(statsRows[0]){
+            switch(wash.service){
+                case 'EXPRESS':
+                    const [expressResults, expressFields] = await connection.query('UPDATE employees_stats SET express_units = express_units - 1 WHERE id = ?', [wash.employee_id]);
+                    const express_average_time = (stats.express_units * stats.express_average_time - wash.time)/(stats.express_units - 1);
+                    const [expressTimeResults, expressTimeFields] = await connection.query('UPDATE employees_stats SET express_average_time = ? WHERE id = ?', [stats.express_units === 1 ? 0 : express_average_time, wash.employee_id]);
+                    break;
+                case 'MASTER':
+                    const [masterResults, masterFields] = await connection.query('UPDATE employees_stats SET master_units = master_units - 1 WHERE id = ?', [wash.employee_id]);
+                    const master_average_time = (stats.master_units * stats.master_average_time - wash.time)/(stats.master_units - 1);
+                    const [masterTimeResults, masterTimeFields] = await connection.query('UPDATE employees_stats SET master_average_time = ? WHERE id = ?', [stats.master_units === 1 ? 0 : master_average_time, wash.employee_id]);
+                    break;
+                case 'PREMIUM':
+                    const [premiumResults, premiumFields] = await connection.query('UPDATE employees_stats SET premium_units = premium_units - 1 WHERE id = ?', [wash.employee_id]);
+                    const premium_average_time = (stats.premium_units * stats.premium_average_time - wash.time)/(stats.premium_units - 1);
+                    const [premiumTimeResults, premiumTimeFields] = await connection.query('UPDATE employees_stats SET premium_average_time = ? WHERE id = ?', [stats.premium_units === 1 ? 0 : premium_average_time, wash.employee_id]);
+                    break;
+                
+            }
+    
+            const [timeResults, timeFields] = await connection.query('UPDATE employees_stats SET total_average_time = (express_average_time + master_average_time + premium_average_time)/3 WHERE id = ?', [wash.employee_id]);
         }
-
-        const [timeResults, timeFields] = await connection.query('UPDATE employees_stats SET total_average_time = (express_average_time + master_average_time + premium_average_time)/3 WHERE id = ?', [wash.employee_id]);
         const [deleteResults, deleteFields] = await connection.query('DELETE FROM washes WHERE id = ?', [wash.id]);
         
         res.send("Wash deleted successfully");
